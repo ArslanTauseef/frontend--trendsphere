35ea92ce3572aedd39d185323e80d94d8bfacb1c
ArsalanTauseef <232355832+ArslanTauseef@users.noreply.github.com>
2025-09-16 14:23:42 +0500

    commit
    new

---
+++ src/components/base/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <header className='ts-header'>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis aliquam dicta ad ut, cum dolore libero laboriosam, vitae consequatur sint, voluptatem illo. Corrupti ipsa inventore excepturi aspernatur cum, iusto provident.
+        </p>
+    </header>
+  )
+}
