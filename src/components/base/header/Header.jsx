a07d3e5233e27e6f11efe14296a7852d23a9da80
ArsalanTauseef <232355832+ArslanTauseef@users.noreply.github.com>
2025-09-20 15:31:05 +0500

    Version #4
    Styling.

--- src/components/base/header/Header.jsx
+++ src/components/base/header/Header.jsx
@@ -1,11 +1,19 @@
-import React from "react";
 import "./Header.css";
+import { FaBattleNet } from "react-icons/fa";
+
+
 
 export const Header = () => {
   return (
     <header className="ts-header">
       <nav className="ts-header-nav">
-        <section className="ts-header-logo">logo</section>
+        <section className="ts-header-logo">
+          <FaBattleNet id="ts-header-logo-icon"/>
+          <div id="ts-header-logo-text">
+            <h1>Trend</h1>
+            <h1>Sphere</h1>
+          </div>
+        </section>
         <section className="ts-header-menu">
           <div>Home</div>
           <div>About</div>
