158e155a31edb7b21571f2051f9e03e73a476ee4
ArsalanTauseef <232355832+ArslanTauseef@users.noreply.github.com>
2025-09-22 17:56:49 +0500

    Fix: keep logo icon above text with z-index

--- src/components/base/header/Header.jsx
+++ src/components/base/header/Header.jsx
@@ -1,17 +1,19 @@
 import "./Header.css";
 import { FaBattleNet } from "react-icons/fa";
 
-
-
 export const Header = () => {
   return (
     <header className="ts-header">
       <nav className="ts-header-nav">
         <section className="ts-header-logo">
-          <FaBattleNet id="ts-header-logo-icon"/>
-          <div id="ts-header-logo-text">
-            <h1>Trend</h1>
-            <h1>Sphere</h1>
+          <div className="ts-header-logo-wrapper">
+            <FaBattleNet id="ts-header-logo-icon" />
+            <div id="ts-header-logo-text-wrapper">
+              <div id="ts-header-logo-text">
+                <h1>TREND</h1>
+                <h1>SPHERE</h1>
+              </div>
+            </div>
           </div>
         </section>
         <section className="ts-header-menu">
