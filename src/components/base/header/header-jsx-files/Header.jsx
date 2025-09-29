c9fec3d2dbee3cfbf6d724ab8562e7c245a6d42d
ArsalanTauseef <232355832+ArslanTauseef@users.noreply.github.com>
2025-09-29 08:31:44 +0500

    Progress #10
    Added slider animation.

--- src/components/base/header/header-jsx-files/Header.jsx
+++ src/components/base/header/header-jsx-files/Header.jsx
@@ -1,24 +1,39 @@
-import { useEffect, useState, useRef } from "react";
+import { useState, useRef, useEffect, useLayoutEffect } from "react";
 import "../header-css-files/Header.css";
 import "../header-css-files/HeaderLogin.css";
 import "../header-css-files/HeaderLogo.css";
 import "../header-css-files/HeaderMenu.css";
 import "../header-css-files/HeaderNav.css";
-
 import { FaBattleNet } from "react-icons/fa";
 
 export const Header = () => {
   const [sliderStyle, setSliderStyle] = useState({});
-  const handleClick = (key) => {
-    const el = menuRefs[key].current
+  const menuRef = useRef(null);
+
+  const handleClick = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const containerRect = menuRef.current.getBoundingClientRect();
+
+    const newStyle = {
+      left: rect.left - containerRect.left + "px",
+      width: rect.width + "px",
+    };
+
+    setSliderStyle(newStyle);
   };
 
-  const HandleNavSlider = () => {
-    console.log("TESTING");
-  };
-
-  useEffect(() => {
-    HandleNavSlider();
+  useLayoutEffect(() => {
+    if (menuRef.current) {
+      const firstItem = menuRef.current.querySelector(".ts-menu");
+      if (firstItem) {
+        const rect = firstItem.getBoundingClientRect();
+        const containerRect = menuRef.current.getBoundingClientRect();
+        setSliderStyle({
+          left: rect.left - containerRect.left + "px",
+          width: rect.width + "px",
+        });
+      }
+    }
   }, []);
 
   return (
@@ -35,12 +50,20 @@ export const Header = () => {
             </div>
           </div>
         </section>
-        <section className="ts-header-menu">
-          {/* <div className="slider"></div> */}
-          <div className="ts-menu">Home</div>
-          <div className="ts-menu">About</div>
-          <div className="ts-menu">Contact</div>
+
+        <section className="ts-header-menu" ref={menuRef}>
+          <div onClick={handleClick} className="ts-menu">
+            Home
+          </div>
+          <div onClick={handleClick} className="ts-menu">
+            About
+          </div>
+          <div onClick={handleClick} className="ts-menu">
+            Contact
+          </div>
+          <div className="slider" style={sliderStyle}></div>
         </section>
+
         <section className="ts-header-login">login section</section>
       </nav>
     </header>
