a9ea701845f72527c9ba4ce05b0f58bad7ee5927
ArsalanTauseef <232355832+ArslanTauseef@users.noreply.github.com>
2025-10-15 13:58:58 +0500

    Progress 24, working on registration page.

--- src/components/pages/login/jsx/Login.jsx
+++ src/components/pages/login/jsx/Login.jsx
@@ -2,8 +2,12 @@ import './../style/ts-login.css'
 
 export const Login = () => {
   return (
-    <section className='ts-login'>
+    <section className='ts-login-main-background'>
+      <div className='ts-login-bordered-container'>
+        <div className='ts-login-main-container'>
 
+        </div>
+      </div>
     </section>
   )
 }
