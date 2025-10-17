975701a367c60873bbd75b6b167809674d79f201
ArsalanTauseef <232355832+ArslanTauseef@users.noreply.github.com>
2025-10-17 14:08:56 +0500

    Progress #27, login page

--- src/components/pages/login/jsx/Login.jsx
+++ src/components/pages/login/jsx/Login.jsx
@@ -1,16 +1,50 @@
-import './../style/ts-login.css'
+import "./../style/ts-login.css";
 
 export const Login = () => {
   return (
-    <section className='ts-login-main-background'>
-      <div className='ts-login-bordered-container'>
-        <div className='ts-login-main-container'>
-
+    <section className="ts-login-main-background">
+      <div className="ts-login-bordered-container">
+        <div className="ts-login-main-container">
+          <div className="ts-form-container sign-in">
+            <form action="">
+              <div>
+                <label htmlFor="">User name</label>
+                <input type="text" />
+              </div>
+              <div>
+                <label htmlFor="">Password</label>
+                <input type="text" />
+              </div>
+              <button>Login</button>
+            </form>
+          </div>
+          <div className="ts-form-container sign-up">
+            <form action="">
+              <div>
+                <label htmlFor="">First name</label>
+                <input type="text" />
+              </div>
+              <div>
+                <label htmlFor="">Last name</label>
+                <input type="text" />
+              </div>
+              <div>
+                <label htmlFor="">Email</label>
+                <input type="text" />
+              </div>
+              <div>
+                <label htmlFor="">Password</label>
+                <input type="text" />
+              </div>
+              <div>
+                <label htmlFor="">Confirm Password</label>
+                <input type="text" />
+              </div>
+              <button>Sign Up</button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
-  )
-}
-
-
-// D
+  );
+};
